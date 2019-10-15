1b3c8d3651b37fff370c2cf7584c8c35f5afeff9
clement-hamon <clementhamon@gmail.com>
2019-10-15 10:06:38 +0200

    display a grid

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ import pygame
 
 screen_width = 300
 screen_height = 600
+block_size = 30
 
 screen = pygame.display.set_mode([screen_width, screen_height])
 screen.fill([0, 0, 0])
+
+
+
+def draw_grid(surface):
+    # draw horizontal lines
+    for i in range(screen_height // block_size):
+        pygame.draw.line(surface, (125, 125, 125), (0, block_size * i),(screen_width, block_size * i))
+    # draw vertical lines
+    for j in range(screen_width // block_size):
+        pygame.draw.line(surface, (125, 125, 125), (block_size * j, 0),(block_size * j, screen_height))
 
 run = True
 while run:
@@ -12,4 +23,5 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    draw_grid(screen)
     pygame.display.update()
